19c6becd4c5c6f0bdbd872ed186a2b6bf2c689fd
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-04-03 13:57:51 +0530

    Update lambda-iam.tf

--- lambda-iam.tf
+++ lambda-iam.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "log_management_statement" {
       "logs:PutLogEvents"
     ]
     resources = [
-      "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:*"
+      "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*"
     ]
   }
 }
