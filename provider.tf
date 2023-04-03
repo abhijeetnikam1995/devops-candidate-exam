5440f09fada72b349d322fc13b3cb73e0d3b1247
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-04-03 11:43:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,14 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
+
+terraform {
+  backend "s3" {
+    bucket         = "3.devops.candidate.exam"
+    key            = "abhijeet.nikam"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+    role_arn       = "arn:aws:iam::<AWS_ACCOUNT_ID_OF_BACKEND>:role/terraform-backend"
+  }
+}
